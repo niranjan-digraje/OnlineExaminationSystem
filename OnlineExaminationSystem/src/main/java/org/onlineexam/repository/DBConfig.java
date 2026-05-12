3992c9766591cdd542b2b7200fa919e1ba882f15
niranjan-digraje <niranjandigraje@gmail.com>
2026-05-12 09:38:53 +0530

    DBConfig file added in repository directory

---
+++ OnlineExaminationSystem/src/main/java/org/onlineexam/repository/DBConfig.java
@@ -0,0 +1,24 @@
+package org.onlineexam.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DBConfig {
+    protected Connection conn;
+    protected PreparedStatement stmt;
+    protected ResultSet rs;
+
+    public DBConfig() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/online_exam_system ", "root", "root");
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Error is DBConfig : " + e);
+        }
+    }
+
+}
